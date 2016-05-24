4f22fc67b7d6739347866ec7e964550c3dfc9493
yangjunpeng <yangjunpeng@readnovel.com>
2016-05-24 16:23:59 +0800

    create project

---
+++ TransitionAnimation/TransitionAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TransitionAnimation
+//
+//  Created by 杨俊鹏 on 16/5/24.
+//  Copyright © 2016年 杨俊鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
